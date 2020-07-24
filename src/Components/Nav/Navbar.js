b028af2a58a56c0ae83b503073da7088b4751ad4
Michael Cortez <CortezDevelopement@gmail.com>
2020-07-24 17:54:32 -0600

    setup basic styles for the navbar component

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default () => {
   return (
-    <div style={{ width: "100%", position: "fixed", top: 0 }}>
+    <div style={{ width: "100%", position: "fixed", top: 0, background: "#f6f6f6" }}>
       <div style={{ width: "100%", height: "80px", display: "flex", alignItems: "center", justifyContent: "center" }}>
         HMK Shop
       </div>
 
-      <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}>
+      <div style={{ width: "100%", height: "40px", display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
         <div>New Arrivals</div>
         <div>All Apparel</div>
         <div>Account</div>
