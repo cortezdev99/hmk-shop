7a02009fa1dbac2e4ca976706a74910c3ed77c42
Michael Cortez <CortezDevelopement@gmail.com>
2020-08-01 23:14:37 -0600

    added a filter section that will soon become its own component for each product page

--- src/Components/New-Arrivals/NewArrivals.js
+++ src/Components/New-Arrivals/NewArrivals.js
@@ -4,24 +4,41 @@ import Products from '../Products/Products'
 export default () => {
   const data = [
     {
-      title: 'Hoodie'
+      title: 'Hoodie',
+      image: 'https://via.placeholder.com/250x250'
     },
     {
-      title: 'Hat'
+      title: 'Hat',
+      image: 'https://via.placeholder.com/250x250'
     },
     {
-      title: 'Sweats'
+      title: 'Sweats',
+      image: 'https://via.placeholder.com/250x250'
     }
   ]
   
   return (
-    <div style={{ height: "100%", width: "100%" }}>
+    <div style={{ height: "calc(100% - 120px)", width: "100%", position: "absolute" }}>
       <div style={{ width: "100%", height: "40%", position: "relative" }}>
         <img
           src="https://via.placeholder.com/250x250"
           alt="placeholder"
           style={{ position: "absolute", width: "100%", height: "100%" }}
         />
+      </div>
+
+      <div style={{ height: "80px", display: "flex", alignItems: "center", justifyContent: "space-evenly" }}>
+        <div>
+          Placeholder
+        </div>
+
+        <div>
+          Placeholder
+        </div>
+        
+        <div>
+          Placeholder
+        </div>
       </div>
 
       <div>
--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 export default (props) => {
+  console.log(props.product.image)
   return (
     <div>
+      <div>
+        <img
+          src={props.product.image}
+          alt="placeholder"
+        />
+      </div>
       {props.product.title}
     </div>
   )
