8fdc08f9396fcadbe34d57b83c7671795b36ff8b
Michael Cortez <CortezDevelopement@gmail.com>
2020-08-03 11:22:50 -0600

    created a dedicated stylesheet for GallerySlider Component and moved styles to it

--- src/Components/Utilities/GallerySlider.js
+++ src/Components/Utilities/GallerySlider.js
@@ -3,41 +3,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default (props) => {
   return (
-    <div style={{ display: "flex", paddingTop: "5px" }}>
-    {
-      props.product.colorImages.length > 3 ? (
-        <div style={{ width: "35px", cursor: "pointer", paddingRight: "5px", fontSize: "35px", display: "flex", alignItems: "center", color: "#7f7f7f" }} onClick={() => props.handleTranslatingImages('Left')}>
-          <FontAwesomeIcon icon="angle-left" />
-        </div>
-      ) : null
-    }
-
-    <div style={{ display: "flex", maxWidth: "180px", overflow: "hidden" }}>
+    <div className="gallery-container">
       {
-        props.product.colorImages.map((color, idx) => {
-          const images = Object.values(color)
-          return images.map((image) => {
-            return (
-              <img
-                style={{ transform: `translate3d(${props.translatePxls}px, 0px, 0px)` }}
-                className="product-gallary-image"
-                key={idx}
-                src={image}
-                onClick={() => props.setImage(Object.values(props.product.colorImages[idx]))}
-              />
-            )
-          })
-        })
+        props.product.colorImages.length > 3 ? (
+          <div
+            className="gallery-left-toggle gallery-toggle"
+            onClick={() => props.handleTranslatingImages('Left')}
+          >
+            <FontAwesomeIcon icon="angle-left" />
+          </div>
+        ) : null
       }
-    </div>
 
-    {
-      props.product.colorImages.length > 3 ? (
-        <div style={{ width: "35px", cursor: "pointer", paddingLeft: "5px", fontSize: "35px", display: "flex", alignItems: "center", justifyContent: "flex-end", color: "#7f7f7f" }}  onClick={() => props.handleTranslatingImages('Right')}>
-          <FontAwesomeIcon icon="angle-right" />
-        </div>
-      ) : null
-    }
+      <div className="gallery-images-container">
+        {
+          props.product.colorImages.map((color, idx) => {
+            const images = Object.values(color)
+            return images.map((image) => {
+              return (
+                <img
+                  style={{ transform: `translate3d(${props.translatePxls}px, 0px, 0px)` }}
+                  className="gallary-images-image"
+                  key={idx}
+                  alt="gallory"
+                  src={image}
+                  onClick={() => props.setImage(Object.values(props.product.colorImages[idx]))}
+                />
+              )
+            })
+          })
+        }
+      </div>
+
+      {
+        props.product.colorImages.length > 3 ? (
+          <div
+            className="gallery-right-toggle gallery-toggle"
+            onClick={() => props.handleTranslatingImages('Right')}
+          >
+            <FontAwesomeIcon icon="angle-right" />
+          </div>
+        ) : null
+      }
   </div>
   )
 }
