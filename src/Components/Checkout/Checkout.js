b29f3d3206efd8e5b89865ec874dc418af454860
Michael Cortez <CortezDevelopement@gmail.com>
2020-08-12 03:17:56 -0600

    setup basic structure and styles for express checkout setion in Checkout Component left grid column

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -16,9 +16,29 @@ export default (props) => {
 
       <div style={{ padding: "0 80px", display: "grid", gridTemplateColumns: "1fr 1fr" }}>
         <div>
-            <div>
-              <div>
-                Express Checkout
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%", paddingTop: "80px" }}>
+              <div style={{ width: "80%", display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+                <span style={{ marginTop: "8px", border: "1px solid #CCC", borderBottom: "none", borderRight: "none" }}></span>
+                <span style={{ textAlign: "center" }}>Express Checkout</span>
+                <span style={{ marginTop: "8px", border: "1px solid #CCC", borderBottom: "none", borderLeft: "none" }}></span>
+              </div>
+
+              <div style={{ width: "80%", display: "flex", alignItems: "center", justifyContent: "center", border: "1px solid #CCC", borderTop: "none", padding: "20px" }}>
+                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button style={{ height: "40px", width: "100%" }}>Google Pay</button>
+                </div>
+
+                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button style={{ height: "40px", width: "100%" }}>Apple Pay</button>
+                </div>
+
+                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button style={{ height: "40px", width: "100%" }}>Amazon Pay</button>
+                </div>
+
+                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button style={{ height: "40px", width: "100%" }}>Paypal</button>
+                </div>
               </div>
             </div>
         </div>
