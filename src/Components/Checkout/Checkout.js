12127c13d317ada59f3ed3651647152c935c5467
Michael Cortez <CortezDevelopement@gmail.com>
2020-08-16 00:30:34 -0600

    added classNames to all elements in checkout component

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -46,53 +46,55 @@ export default () => {
   }, [products])
 
   return (
-    <div id="checkout-container" style={{ paddingBottom: "80px" }}>
-      <div>
+    <div className="checkout-container"  id="checkout-container" style={{ paddingBottom: "80px" }}>
+      <div className="checkout-banner-image-wrapper">
         <img
+          className="checkout-banner-image"
           src="https://via.placeholder.com/1900x646"
           style={{ height: "40%", maxHeight: "200px", backgroundSize: "cover", width: "100%" }}
           alt="bannerImage"
         />
       </div>
 
-      <div style={{ padding: "0 80px", display: "grid", gridTemplateColumns: "1.5fr 1fr", gridGap: "80px" }}>
-        <div>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%", paddingTop: "72px" }}>
-              <div style={{ width: "100%", display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
-                <span style={{ marginTop: "8px", border: "1px solid #CCC", borderBottom: "none", borderRight: "none" }}></span>
-                <span style={{ textAlign: "center" }}>Express Checkout</span>
-                <span style={{ marginTop: "8px", border: "1px solid #CCC", borderBottom: "none", borderLeft: "none" }}></span>
+      <div className="checkout-wrapper" style={{ padding: "0 80px", display: "grid", gridTemplateColumns: "1.5fr 1fr", gridGap: "80px" }}>
+        <div className="checkout-left-column">
+            <div className="checkout-express-checkout-wrapper" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%", paddingTop: "72px" }}>
+              <div className="checkout-express-checkout-header-wrapper" style={{ width: "100%", display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+                <span className="checkout-express-checkout-header-border" style={{ marginTop: "8px", border: "1px solid #CCC", borderBottom: "none", borderRight: "none" }}></span>
+                <span className="checkout-express-checkout-header" style={{ textAlign: "center" }}>Express Checkout</span>
+                <span className="checkout-express-checkout-header-border" style={{ marginTop: "8px", border: "1px solid #CCC", borderBottom: "none", borderLeft: "none" }}></span>
               </div>
 
-              <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", border: "1px solid #CCC", borderTop: "none", padding: "20px" }}>
-                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
-                  <button style={{ height: "40px", width: "100%" }}>Google Pay</button>
+              <div className="checkout-express-checkout-btns-wrapper" style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", border: "1px solid #CCC", borderTop: "none", padding: "20px" }}>
+                <div className="checkout-express-checkout-btn-wrapper" style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button className="checkout-express-checkout-btn" style={{ height: "40px", width: "100%" }}>Google Pay</button>
                 </div>
 
-                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
-                  <button style={{ height: "40px", width: "100%" }}>Apple Pay</button>
+                <div className="checkout-express-checkout-btn-wrapper" style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button className="checkout-express-checkout-btn" style={{ height: "40px", width: "100%" }}>Apple Pay</button>
                 </div>
 
-                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
-                  <button style={{ height: "40px", width: "100%" }}>Amazon Pay</button>
+                <div className="checkout-express-checkout-btn-wrapper" style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button className="checkout-express-checkout-btn" style={{ height: "40px", width: "100%" }}>Amazon Pay</button>
                 </div>
 
-                <div style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
-                  <button style={{ height: "40px", width: "100%" }}>Paypal</button>
+                <div className="checkout-express-checkout-btn-wrapper" style={{ paddingLeft: "5px", paddingRight: "5px", width: "140px" }}>
+                  <button className="checkout-express-checkout-btn" style={{ height: "40px", width: "100%" }}>Paypal</button>
                 </div>
               </div>
             </div>
 
-            <div style={{ paddingTop: "80px", display: "grid", gridTemplateColumns: "2fr 1fr 2fr" }}>
-              <span style={{ marginTop: "8px", borderTop: "1px solid #CCC" }}></span>
-              <span style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>OR</span>
-              <span style={{ marginTop: "8px", borderTop: "1px solid #CCC" }}></span>
+            <div className="checkout-options-seperator-wrapper" style={{ paddingTop: "80px", display: "grid", gridTemplateColumns: "2fr 1fr 2fr" }}>
+              <span className="checkout-options-seperator-border" style={{ marginTop: "8px", borderTop: "1px solid #CCC" }}></span>
+              <span className="checkout-options-seperator-header" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>OR</span>
+              <span className="checkout-options-seperator-border" style={{ marginTop: "8px", borderTop: "1px solid #CCC" }}></span>
             </div>
 
-            <div style={{ width: "100%", paddingTop: "80px" }}>
-              <div style={{ fontSize: "18px" }}>Contact Information</div>
-                <div style={{ paddingTop: "20px" }}>
+            <div className="checkout-contact-info-wrapper" style={{ width: "100%", paddingTop: "80px" }}>
+              <div className="checkout-contact-info-header" style={{ fontSize: "18px" }}>Contact Information</div>
+                <div className="checkout-contact-info-input-wrapper" style={{ paddingTop: "20px" }}>
                   <input
+                    className="checkout-input"
                     placeholder="Email"
                     type="email"
                     value={email}
@@ -101,8 +103,9 @@ export default () => {
                   />
                 </div>
 
-                <div style={{ paddingTop: "20px", display: "flex" }}>
+                <div className="checkout-contact-info-input-wrapper" style={{ paddingTop: "20px", display: "flex" }}>
                   <input
+                    className="checkout-input"
                     placeholder="Phone"
                     type="tel"
                     value={phone}
@@ -112,31 +115,32 @@ export default () => {
                 </div>
             </div>
 
-            <div style={{ width: "100%", paddingTop: "80px" }}>
-              <div style={{ paddingBottom: "20px", fontSize: "18px" }}>Shipping address</div>
+            <div className="checkout-shipping-info-wrapper" style={{ width: "100%", paddingTop: "80px" }}>
+              <div className="checkout-shipping-info-header" style={{ paddingBottom: "20px", fontSize: "18px" }}>Shipping address</div>
 
-              <div>
-                <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", columnGap: "20px" }}>
-                  <input
-                    placeholder="First name"
-                    type="text"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    style={{ width: "100%", height: "50px" }}
-                  />
+              <div className="checkout-shipping-info-name-wrapper" style={{ display: "grid", gridTemplateColumns: "1fr 1fr", columnGap: "20px" }}>
+                <input
+                  className="checkout-input"
+                  placeholder="First name"
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  style={{ width: "100%", height: "50px" }}
+                />
 
-                  <input
-                    placeholder="Last name"
-                    type="text"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    style={{ width: "100%", height: "50px" }}
-                  />
-                </div>
+                <input
+                  className="checkout-input"
+                  placeholder="Last name"
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  style={{ width: "100%", height: "50px" }}
+                />
               </div>
 
-              <div style={{ paddingTop: "20px" }}>
+              <div className="checkout-shipping-info-address-one-wrapper  checkout-input-padding" style={{ paddingTop: "20px" }}>
                 <input
+                    className="checkout-input"
                     placeholder="Address"
                     type="text"
                     value={address}
@@ -145,8 +149,9 @@ export default () => {
                   />
               </div>
 
-              <div style={{ paddingTop: "20px" }}>
+              <div className="checkout-shipping-info-address-two-wrapper checkout-input-padding" style={{ paddingTop: "20px" }}>
                 <input
+                    className="checkout-input"
                     placeholder="Apartment, Suite, etc. (optional)"
                     type="text"
                     value={address2}
@@ -155,8 +160,9 @@ export default () => {
                   />
               </div>
 
-              <div style={{ paddingTop: "20px" }}>
+              <div className="checkout-shipping-info-city-wrapper checkout-input-padding" style={{ paddingTop: "20px" }}>
                 <input
+                    className="checkout-input"
                     placeholder="City"
                     type="text"
                     value={city}
@@ -165,12 +171,13 @@ export default () => {
                   />
               </div>
 
-                <div style={{ paddingTop: "20px", display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "20px" }}>
+                <div className="checkout-shipping-info-region-state-city-wrapper checkout-input-padding" style={{ paddingTop: "20px", display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "20px" }}>
                   <CountryDropdown 
                     setRegion={(country) => setRegion(country)}
                   />
 
                   <input
+                    className="checkout-input"
                     placeholder="State"
                     type="text"
                     value={state}
@@ -179,6 +186,7 @@ export default () => {
                   />
 
                   <input
+                    className="checkout-input"
                     placeholder="Zip code"
                     type="text"
                     value={zip}
@@ -188,8 +196,9 @@ export default () => {
                 </div>
             </div>
 
-            <div style={{ height: "70px", paddingTop: "20px", display: "flex", justifyContent: "space-between" }}>
+            <div className="checkout-left-column-btns-wrapper" style={{ height: "70px", paddingTop: "20px", display: "flex", justifyContent: "space-between" }}>
               <Link
+                className="checkout-left-column-left-link"
                 style={{ display: "flex", justifyContent: "center", alignItems: "center", textDecoration: "none", color: "#1d1d1d" }}
                 to="/"
                 onClick={() => {
@@ -200,11 +209,12 @@ export default () => {
                   navbarElement.classList.toggle('hidden-nav')
                 }}
               >
-                <span style={{ paddingRight: "5px", fontSize: "14px" }}><FontAwesomeIcon icon="arrow-left" /></span> Return to home
+                <span className="checkout-left-column-link-icon" style={{ paddingRight: "20px", fontSize: "14px" }}><FontAwesomeIcon icon="arrow-left" /></span> Return to home
               </Link>
 
               <div>
                 <Link
+                  className="checkout-left-column-right-link"
                   style={{ height: "100%", width: "100%", padding: "1em", display: "flex", alignItems: "center", justifyContent: "center", textDecoration: "none", color: "#1d1d1d", backgroundColor: "#45b3e0", borderRadius: "5px" }}
                   to={{
                     pathname: `/checkout/payment`,
@@ -235,39 +245,40 @@ export default () => {
             </div>
         </div>
 
-        <div style={{ paddingTop: "40px" }}>
-          <div>
+        <div className="checkout-right-column-wrapper" style={{ paddingTop: "40px" }}>
+          <div className="checkout-products-wrapper">
             {
               products.map((product, productIdx) => {
                 return (
-                  <div key={productIdx} style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "5px", padding: "40px 0", borderBottom: "1px solid #CCC" }}>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      <div style={{ width: "100px", height: "100px", display: "flex" }}>
+                  <div className="checkout-product-wrapper" key={productIdx} style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "5px", padding: "40px 0", borderBottom: "1px solid #CCC" }}>
+                    <div className="checkout-product-image-container" style={{ display: "flex", justifyContent: "center" }}>
+                      <div className="checkout-product-image-wrapper" style={{ width: "100px", height: "100px", display: "flex" }}>
                         <img
                           src={product[3].image}
                           style={{ width: "100%", height: "100%" }}
                           alt="productImage"
+                          className="checkout-product-image"
                         />
 
-                        <div style={{ position: "relative" }}>
-                          <div style={{ width: "25px", height: "25px", position: "absolute", top: "-12px", right: "-12px", backgroundColor: "rgba(114,114,114,0.9)", color: "white", borderRadius: "50%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <div className="checkout-product-image-quantity-wrapper" style={{ position: "relative" }}>
+                          <div className="checkout-product-image-quantity" style={{ width: "25px", height: "25px", position: "absolute", top: "-12px", right: "-12px", backgroundColor: "rgba(114,114,114,0.9)", color: "white", borderRadius: "50%", display: "flex", alignItems: "center", justifyContent: "center" }}>
                             {product[4].quantity}
                           </div>
                         </div>
                       </div>
                     </div>
 
-                    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                      <div style={{ textAlign: "center" }}>
+                    <div className="checkout-product-info-wrapper" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                      <div className="checkout-product-title" style={{ textAlign: "center" }}>
                         {product[0].product.title}
                       </div>
 
-                      <div style={{ textAlign: "center" }}>
+                      <div className="checkout-product-color" style={{ textAlign: "center" }}>
                         {product[2].color} / {product[1].size.toUpperCase()}
                       </div>
                     </div>
 
-                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <div className="checkout-product-price" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                       ${product[0].product.price * product[4].quantity}
                     </div>
                   </div>
@@ -276,17 +287,19 @@ export default () => {
             }
           </div>
 
-          <div style={{ display: "grid", gridTemplateColumns: "2fr 1fr", columnGap: "20px", padding: "40px 0", borderBottom: "1px solid #CCC" }}>
-            <div>
+          <div className="checkout-discount-code-wrapper" style={{ display: "grid", gridTemplateColumns: "2fr 1fr", columnGap: "20px", padding: "40px 0", borderBottom: "1px solid #CCC" }}>
+            <div className="checkout-discount-input-wrapper">
               <input
+                className="checkout-input"
                 placeholder="Discount or promo code"
                 type="text"
                 style={{ width: "100%", height: "50px" }}
               />
             </div>
 
-            <div>
+            <div className="checkout-discount-btn-wrapper">
               <button
+                className="checkout-discount-btn"
                 style={{ width: "100%", height: "50px" }}
               >
                 Apply
@@ -294,34 +307,34 @@ export default () => {
             </div>
           </div>
 
-          <div style={{ padding: "40px 0", borderBottom: "1px solid #CCC" }}>
-            <div style={{ display: "flex", justifyContent: "space-between", paddingBottom: "20px" }}>
-              <div style={{ fontSize: "14px" }}>
+          <div className="checkout-total-calculations-wrapper" style={{ padding: "40px 0", borderBottom: "1px solid #CCC" }}>
+            <div className="checkout-subtotal-wrapper" style={{ display: "flex", justifyContent: "space-between", paddingBottom: "20px" }}>
+              <div className="checkout-subtotal-header" style={{ fontSize: "14px" }}>
                 Subtotal
               </div>
 
-              <div>
+              <div className="checkout-subtotal-price">
                 ${subtotal}
               </div>
             </div>
 
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <div style={{ fontSize: "14px" }}>
+            <div className="checkout-shipping-wrapper" style={{ display: "flex", justifyContent: "space-between" }}>
+              <div className="checkout-shipping-header" style={{ fontSize: "14px" }}>
                 Shipping
               </div>
 
-              <div>
+              <div className="checkout-shipping-price">
                 { region === 'USA' ? "$6" : region === "" ? "Not calculated yet." : "$8" }
               </div>
             </div>
           </div>
 
-          <div style={{ paddingTop: "40px", display: "flex", justifyContent: "space-between" }}>
-            <div>
+          <div className="checkout-total-wrapper" style={{ paddingTop: "40px", display: "flex", justifyContent: "space-between" }}>
+            <div className="checkout-total-header">
               Total
             </div>
 
-            <div>
+            <div className="checkout-total-price">
               {region === "USA" ? `$${subtotal + 6}` : region === "" ? "Not calculated yet." : `$${subtotal + 8}`}
             </div>
           </div>
