96037a218017df3a9075aa46b64bcc063d499dc9
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-10 17:11:19 -0600

    fixed gallary image proportions on mobile devices smaller than 640px but breaks around 300px

--- src/Components/Utilities/GallerySlider.js
+++ src/Components/Utilities/GallerySlider.js
@@ -1,27 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default (props) => {
   const [translatePxls, setTranslatePxls] = useState(0)
   const [activeGalloryIdx, setActiveGalloryIdx] = useState(2)
+  // let windowWidth
+  const [windowWidth, setWindowWidth] = useState(null)
 
   const handleTranslatingImages = (direction) => {
-    if (direction === 'Left' && activeGalloryIdx !== 2) {
+    if (direction === 'Left' && activeGalloryIdx !== 2 && windowWidth > 640) {
       setActiveGalloryIdx(activeGalloryIdx - 1)
       return setTranslatePxls(translatePxls + 60)
-    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length - 1) {
+    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length - 1 && windowWidth > 640) {
       setActiveGalloryIdx(activeGalloryIdx + 1)
       setTranslatePxls(translatePxls - 60)
+    } else if (direction === 'Left' && activeGalloryIdx !== 2 && windowWidth <= 640) {
+      setActiveGalloryIdx(activeGalloryIdx - 1)
+      return setTranslatePxls(translatePxls + 55)
+    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length && windowWidth <= 640) {
+      setActiveGalloryIdx(activeGalloryIdx + 1)
+      setTranslatePxls(translatePxls - 55)
     }
   }
+
+  console.log(windowWidth)
+
+  useEffect(() => {
+    setWindowWidth(window.document.body.clientWidth)
+  }, [ window.document.body.clientWidth ])
 
   return (
     <div className="gallery-container">
       {
-        props.product.images.length > 3 ? (
+        windowWidth > 640 && props.product.images.length > 3 ? (
           <div
             className="gallery-left-toggle gallery-toggle"
             onClick={() => handleTranslatingImages('Left')}
+          >
+            <FontAwesomeIcon icon="angle-left" />
+          </div>
+        ) : windowWidth < 640 && props.product.images.length > 2 ? (
+          <div
+          className="gallery-left-toggle gallery-toggle"
+          onClick={() => handleTranslatingImages('Left')}
           >
             <FontAwesomeIcon icon="angle-left" />
           </div>
@@ -49,7 +70,14 @@ export default (props) => {
       </div>
 
       {
-        props.product.images.length > 3 ? (
+         windowWidth > 640 && props.product.images.length > 3 ? (
+          <div
+            className="gallery-right-toggle gallery-toggle"
+            onClick={() => handleTranslatingImages('Right')}
+          >
+            <FontAwesomeIcon icon="angle-right" />
+          </div>
+        ) : windowWidth < 640 && props.product.images.length > 2 ? (
           <div
             className="gallery-right-toggle gallery-toggle"
             onClick={() => handleTranslatingImages('Right')}
