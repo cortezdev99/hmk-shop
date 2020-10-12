0eb891fae4823625c338c7d85847b64c7b883726
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-12 00:09:21 -0600

    fixed naming conventions in gallery component to all be spelled correctly and match each other

--- src/Components/Utilities/GallerySlider.js
+++ src/Components/Utilities/GallerySlider.js
@@ -3,34 +3,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default (props) => {
   const [translatePxls, setTranslatePxls] = useState(0)
-  const [activeGalloryIdx, setActiveGalloryIdx] = useState(2)
+  const [activeGalleryIdx, setActiveGalleryIdx] = useState(2)
   const [prevWindowWidth, setPrevWindowWidth] = useState(window.document.body.clientWidth)
   const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
   const handleTranslatingImages = (direction) => {
-    if (direction === 'Left' && activeGalloryIdx !== 2 && window.document.body.clientWidth > 640) {
-      setActiveGalloryIdx(activeGalloryIdx - 1)
+    if (direction === 'Left' && activeGalleryIdx !== 2 && window.document.body.clientWidth > 640) {
+      setActiveGalleryIdx(activeGalleryIdx - 1)
       return setTranslatePxls(translatePxls + 60)
-    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length - 1 && window.document.body.clientWidth > 640) {
-      setActiveGalloryIdx(activeGalloryIdx + 1)
+    } else if (direction === 'Right' && activeGalleryIdx < props.product.images.length - 1 && window.document.body.clientWidth > 640) {
+      setActiveGalleryIdx(activeGalleryIdx + 1)
       setTranslatePxls(translatePxls - 60)
-    } else if (direction === 'Left' && activeGalloryIdx !== 2 && window.document.body.clientWidth <= 640 && window.document.body.clientWidth > 375) {
-      setActiveGalloryIdx(activeGalloryIdx - 1)
+    } else if (direction === 'Left' && activeGalleryIdx !== 2 && window.document.body.clientWidth <= 640 && window.document.body.clientWidth > 375) {
+      setActiveGalleryIdx(activeGalleryIdx - 1)
       return setTranslatePxls(translatePxls + 55)
-    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length && window.document.body.clientWidth <= 640 && window.document.body.clientWidth > 375) {
-      setActiveGalloryIdx(activeGalloryIdx + 1)
+    } else if (direction === 'Right' && activeGalleryIdx < props.product.images.length && window.document.body.clientWidth <= 640 && window.document.body.clientWidth > 375) {
+      setActiveGalleryIdx(activeGalleryIdx + 1)
       setTranslatePxls(translatePxls - 55)
-    } else if (direction === 'Left' && activeGalloryIdx !== 2 && window.document.body.clientWidth <= 375 && window.document.body.clientWidth > 325) {
-      setActiveGalloryIdx(activeGalloryIdx - 1)
+    } else if (direction === 'Left' && activeGalleryIdx !== 2 && window.document.body.clientWidth <= 375 && window.document.body.clientWidth > 325) {
+      setActiveGalleryIdx(activeGalleryIdx - 1)
       return setTranslatePxls(translatePxls + 46.5)
-    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length && window.document.body.clientWidth <= 375  && window.document.body.clientWidth > 325) {
-      setActiveGalloryIdx(activeGalloryIdx + 1)
+    } else if (direction === 'Right' && activeGalleryIdx < props.product.images.length && window.document.body.clientWidth <= 375  && window.document.body.clientWidth > 325) {
+      setActiveGalleryIdx(activeGalleryIdx + 1)
       setTranslatePxls(translatePxls - 46.5)
-    } else if (direction === 'Left' && activeGalloryIdx !== 2 && window.document.body.clientWidth <= 325) {
-      setActiveGalloryIdx(activeGalloryIdx - 1)
+    } else if (direction === 'Left' && activeGalleryIdx !== 2 && window.document.body.clientWidth <= 325) {
+      setActiveGalleryIdx(activeGalleryIdx - 1)
       return setTranslatePxls(translatePxls + 38.19)
-    } else if (direction === 'Right' && activeGalloryIdx < props.product.images.length && window.document.body.clientWidth <= 325) {
-      setActiveGalloryIdx(activeGalloryIdx + 1)
+    } else if (direction === 'Right' && activeGalleryIdx < props.product.images.length && window.document.body.clientWidth <= 325) {
+      setActiveGalleryIdx(activeGalleryIdx + 1)
       setTranslatePxls(translatePxls - 38.19)
     }
   }
@@ -39,12 +39,12 @@ export default (props) => {
       window.addEventListener('resize', (event) => {
         if (window.document.body.clientWidth > 640 && prevWindowWidth < 640) {
           setPrevWindowWidth(window.document.body.clientWidth)
-          setActiveGalloryIdx(2)
+          setActiveGalleryIdx(2)
           setTranslatePxls(0)
           forceUpdate()
         } else if (window.document.body.clientWidth < 640 && prevWindowWidth > 640) {
           setPrevWindowWidth(window.document.body.clientWidth)
-          setActiveGalloryIdx(2)
+          setActiveGalleryIdx(2)
           setTranslatePxls(0)
           forceUpdate()
         }
@@ -79,9 +79,9 @@ export default (props) => {
               return (
                 <img
                   style={{ transform: `translate3d(${translatePxls}px, 0px, 0px)` }}
-                  className="gallary-images-image"
+                  className="gallery-images-image"
                   key={idx}
-                  alt="gallory"
+                  alt="Gallery"
                   src={Object.values(image[0])[0]}
                   onClick={() => props.setImage(Object.values(Object.values(props.product.images[idx])[0][0])[0])}
                 />
