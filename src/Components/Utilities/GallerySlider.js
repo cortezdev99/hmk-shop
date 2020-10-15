ab008d21673b1a8636d3b538a715d882d80c1588
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-15 17:06:17 -0600

    added a cleanup function to get rid of Gallery Slider components event listener, also added more logic to check for resized gallery image

--- src/Components/Utilities/GallerySlider.js
+++ src/Components/Utilities/GallerySlider.js
@@ -73,6 +73,8 @@ export default props => {
     }
   };
 
+ 
+
   useEffect(() => {
     window.addEventListener("resize", event => {
       if (activeGalleryIdx === 2 || translatePxls === 0) {
@@ -92,8 +94,32 @@ export default props => {
         setActiveGalleryIdx(2);
         setTranslatePxls(0);
         forceUpdate();
+      } else if (window.document.body.clientWidth > 375 && prevWindowWidth <= 375) {
+        setPrevWindowWidth(window.document.body.clientWidth);
+        setActiveGalleryIdx(2);
+        setTranslatePxls(0);
+        forceUpdate();
+      } else if (window.document.body.clientWidth <= 375 && prevWindowWidth > 375) {
+        setPrevWindowWidth(window.document.body.clientWidth);
+        setActiveGalleryIdx(2);
+        setTranslatePxls(0);
+        forceUpdate();
+      } else if (window.document.body.clientWidth > 325 && prevWindowWidth <= 325) {
+        setPrevWindowWidth(window.document.body.clientWidth);
+        setActiveGalleryIdx(2);
+        setTranslatePxls(0);
+        forceUpdate();
+      } else if (window.document.body.clientWidth <= 325 && prevWindowWidth > 325) {
+        setPrevWindowWidth(window.document.body.clientWidth);
+        setActiveGalleryIdx(2);
+        setTranslatePxls(0);
+        forceUpdate();
       }
     });
+
+    return () => {
+      window.removeEventListener("resize", null)
+    }
   });
 
   return (
