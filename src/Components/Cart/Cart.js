57c3a3fd2b90eaa08b55a4e3a4f54604f78be415
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-29 21:39:40 -0600

    added new color scheme to cart component, found issues with cart component on mobile where scrollbar does not activate when device rotated

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -70,23 +70,49 @@ export default () => {
 
   return (
     <div className="cart-container">
-      <div className="cart-wrapper" id="cart-wrapper">
-        <div className="cart-heading-wrapper">
-          <div className="cart-heading">
-            Cart
+      <div className="cart-wrapper" id="cart-wrapper" style={{
+        overflowX: "hidden",
+        background: "linear-gradient(90deg,#fff,#fbfbfb,#ccc)",
+      }}>
+        <div className="cart-heading-wrapper" style={{
+          borderBottom: "1px solid transparent",
+          background: "#fff"
+        }}>
+          <div className="cart-heading" style={{ transform: "scaleX(-1)" }}>
+            {/* <FontAwesomeIcon icon={["fas", "shopping-cart"]} /> */}
           </div>
 
           <div
             className="cart-heading-close-btn"
             onClick={handleCloseModal}
+            style={{
+              width: "25px",
+              height: "25px"
+            }}
           >
-            <FontAwesomeIcon icon="times" />
+            <svg
+              style={{ width: "100%", height: "100%" }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-x"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
           </div>
         </div>
 
         {
           products.length > 0 ? (
-            <div className="cart-products-container">
+            <div className="cart-products-container" style={{
+              // background: "linear-gradient(90deg,#fff,#fbfbfb,#ccc)",
+              // zIndex: 2
+            }}>
               {
                 products.map((product, productIdx) => {
                   return (
@@ -166,7 +192,9 @@ export default () => {
           )
         }
 
-        <div className="cart-product-checkout-wrapper" id="cart-product-checkout-wrapper">
+        <div className="cart-product-checkout-wrapper" id="cart-product-checkout-wrapper" style={{
+          // background: "linear-gradient(90deg,#fff,#fbfbfb,#ccc)"
+        }}>
           {
             firebase.auth().currentUser === null ? (
               <Link
