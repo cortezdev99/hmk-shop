7ca3bd26e5fc303ae209102dd781ef28453934bf
Michael Cortez <CortezDevelopement@gmail.com>
2020-11-22 22:12:38 -0700

    now setting the region in checkout component to the selected region in country dropdown component, showing the user the region they selected in country dropdown component, and collapsing country dropdown when a region is selected

--- src/Components/Checkout/CountryDropdown.js
+++ src/Components/Checkout/CountryDropdown.js
@@ -1,11 +1,70 @@
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
 
 export default (props) => {
-  const handleCollapsableContent = () => {
-    const el = document.getElementById("country-dropdown-collapsable-content-wrapper")
-    if (el !== null) {
+  const [ selectedCountry, setSelectedCountry ] = useState(null)
+  const [ countries, setCountries ] = useState([
+    {
+      "country": "Australia",
+      "alpha_2_country_code": "AU"
+     },
+     {
+      "country": "Brazil",
+      "alpha_2_country_code": "BR"
+     },
+     {
+      "country": "Canada",
+      "alpha_2_country_code": "CA"
+     },
+     {
+      "country": "France",
+      "alpha_2_country_code": "FR"
+     },
+     {
+      "country": "Italy",
+      "alpha_2_country_code": "IT"
+     },
+     {
+      "country": "Spain",
+      "alpha_2_country_code": "ES"
+     },
+     {
+      "country": "Sweden",
+      "alpha_2_country_code": "SE"
+     },
+     {
+      "country": "United Kingdom",
+      "alpha_2_country_code": "GB"
+     },
+     {
+      "country": "United States of America",
+      "alpha_2_country_code": "US"
+     }
+  ])
+
+  const el = document.getElementById("country-dropdown-collapsable-content-wrapper")
+  const el2 = document.getElementById("checkout-shipping-info-wrapper")
+  const el3 = document.getElementById("country-dropdown-chevron")
+
+  const handleSettingSelectedCountry = (idxOfCountry) => {
+    setSelectedCountry(countries[idxOfCountry].country)
+    props.setRegion([
+      countries[idxOfCountry].country,
+      countries[idxOfCountry].alpha_2_country_code
+    ])
+    
+    if (el !== null && el2 !== null && el3 !== null) {
+      el2.classList.toggle("transform-add-shipping-inner-content-maximum")
       el.classList.toggle('country-dropdown-collapsable-content-showing')
+      el3.classList.toggle('country-dropdown-chevron-rotated')
+    }
+  }
+
+  const handleCollapsableContent = () => {
+    if (el !== null && el2 !== null && el3 !== null) {
+      el2.classList.toggle("transform-add-shipping-inner-content-maximum")
+      el.classList.toggle('country-dropdown-collapsable-content-showing')
+      el3.classList.toggle('country-dropdown-chevron-rotated')
     }
   }
 
@@ -19,29 +78,39 @@ export default (props) => {
       color: "#7c7979",
       borderRadius: "5px",
       font: "500 13.3333px Arial",
-      "-webkit-font-smoothing": "auto",
+      WebkitFontSmoothing: "auto",
       letterSpacing: "0.75px",
       overflow: "hidden"
     }}
-      onClick={
-        handleCollapsableContent
-      }
     >
-        <div style={{
+        <div 
+          style={{
           width: "100%",
           height: "45px",
           padding: "0 7px",
-          boxShadow: "#0f0f0f 1px 0px 12px -4px",
+          boxShadow: "-157px 5px 25px -14px rgba(50, 50, 50, 0.75), 147px 5px 25px -14px rgba(50, 50, 50, 0.75)",
           display: "flex",
           alignItems: "center",
           justifyContent: "center"
-        }}>
-          <div>
-            Country or Region
+          }}
+
+          onClick={
+            handleCollapsableContent
+          }
+        >
+          <div style={{
+            paddingRight: "10px"
+          }}>
+            {
+              selectedCountry === null ? "Country or Region" : selectedCountry
+            }
           </div>
 
-          <div style={{
-            paddingLeft: "10px"
+          <div
+            id="country-dropdown-chevron"
+            className="country-dropdown-chevron"
+            style={{
+            transition: "transform 0.7s ease-in-out"
           }}>
             <FontAwesomeIcon icon={["fas", "chevron-down"]} />
           </div>
@@ -57,126 +126,40 @@ export default (props) => {
             transition: "max-height 0.7s"
           }}
         >
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Australia
-          </div>
 
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Brazil
-          </div>
+          {
+            countries.map((country, idxOfCountry) => {
+              return (
+              <div 
+                key={idxOfCountry}
+                style={{
+                  height: "45px",
+                  borderTop: "1px solid #CCC",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }} 
 
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Canada
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            France
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Germany
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Italy
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Spain
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            Sweden
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            United Kingdom
-          </div>
-
-          <div style={{
-            height: "45px",
-            borderTop: "1px solid #CCC",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            United States of America
-          </div>
+                onClick={ () => handleSettingSelectedCountry(idxOfCountry) }
+              >
+                {
+                  country.country
+                }
+              </div>
+              )
+            })
+          }
         </div>
-      {/* </div> */}
     </div>
-    // <select
-    //   id="country"
-    //   className="country-select-element"
-    //   name="country"
-    //   style={{ width: "100%", height: "45px", borderRadius: "5px", border: "1px solid #CCC", paddingLeft: "15px" }}
-    //   onChange={(e) => props.setRegion(e.target.value)}
-    // >
-      // <option disabled selected>Country or Region</option>
-      // <option className="country-selection-option" value={["Australia", "AU"]}>Australia</option>
-      // <option className="country-selection-option" value={["Brazil", "BR"]}>Brazil</option>
-      // <option className="country-selection-option" value={["Canada", "CA"]}>Canada</option>
-      // <option className="country-selection-option" value={["France", "FR"]}>France</option>
-      // <option className="country-selection-option" value={["Germany", "DE"]}>Germany</option>
-      // <option className="country-selection-option" value={["Italy", "IT"]}>Italy</option>
-      // <option className="country-selection-option" value={["Spain", "ES"]}>Spain</option>
-      // <option className="country-selection-option" value={["Sweden", "SE"]}>Sweden</option>
-      // <option className="country-selection-option" value={["United Kingdom", "GB"]}>United Kingdom</option>
-      // <option className="country-selection-option" value={["United States of America", "US"]}>United States of America</option>
-    // </select>
+      // {["Australia", "AU"]}
+      // {["Brazil", "BR"]}
+      // {["Canada", "CA"]}
+      // {["France", "FR"]}
+      // {["Germany", "DE"]}
+      // {["Italy", "IT"]}
+      // {["Spain", "ES"]}
+      // {["Sweden", "SE"]}
+      // {["United Kingdom", "GB"]}
+      // {["United States of America", "US"]}
   )
 }
