b31b71fc66cc06179d401776d948995488081654
Michael Cortez <CortezDevelopement@gmail.com>
2020-12-20 21:18:06 -0700

    finished logic for moreInfoIcon for current usecase, now going to rename variables to actually explain what they are

--- src/Components/Utilities/MoreInfoIcon.js
+++ src/Components/Utilities/MoreInfoIcon.js
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, {useState, useEffect} from 'react'
 
-export default () => {
+export default (props) => {
   const el = window.document.getElementById('more-info-icon-container')
+  const el2 = window.document.getElementById('more-info-popup-content')
   const [test, setTest] = useState(false)
+  const [offset, setOffset] = useState(50)
   const handleTest = (e) => {
     e.stopPropagation()
+
     setTest(true)
 
     setTimeout(() => {
@@ -25,6 +28,14 @@ export default () => {
     }
   })
 
+  useEffect(() => {
+    if (el2) {
+      if (el2.clientHeight !== offset) {
+        setOffset(el2.clientHeight);
+      }
+    }
+  })
+
 
   return (
     <div 
@@ -35,51 +46,48 @@ export default () => {
       justifyContent: "center",
       marginTop: "2px"
     }}>
-      {
-        test ? (
-          <div style={{
-            position: "absolute",
-            top: "-50px",
+        <div 
+          id="more-info-popup-content"
+          style={{
+          position: "absolute",
+          top: `-${offset}px`,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginLeft: "5px",
+          visibility: `${test ? "visible" : "hidden"}`
+        }}>
+          <div  style={{
+            background: "#333333e6",
+            color: "#fff",
+            borderRadius: "5px",
+            fontWeight: "600",
+            padding: "5px",
+            minWidth: "100px",
+            maxWidth: "175px",
+            minHeight: "50px",
             display: "flex",
             alignItems: "center",
-            justifyContent: "center",
-            marginLeft: "5px"
+            justifyContent: "center"
           }}>
-            <div  style={{
-              background: "#333333e6",
-              // background: "#1c1b1be6",
-              color: "#fff",
-              borderRadius: "5px",
-              padding: "5px",
-              
-              minWidth: "100px",
-              maxWidth: "175px",
-              minHeight: "50px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}>
-              <div style={{
-                marginBottom: "2px",
-                textAlign: "center"
-              }}>
-                test
-              </div>
-            </div>
-
             <div style={{
-              position: "absolute",
-              top: "100%",
-              width: 0,
-              border: "6px solid transparent",
-              borderTopColor: "#333333e6"
-              // borderTopColor: "#1c1b1be6"
+              marginBottom: "2px",
+              textAlign: "center"
             }}>
-              
+              {props.info}
             </div>
           </div>
-        ) : null
-      }
+
+          <div style={{
+            position: "absolute",
+            top: "100%",
+            width: 0,
+            border: "6px solid transparent",
+            borderTopColor: "#333333e6"
+          }}>
+            
+          </div>
+        </div>
 
       <div style={{
         paddingLeft: "5px",
