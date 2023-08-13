61d73c4d5dd4ffe9337d8d46a62206cdbd6eb1e1
Michael Cortez <CortezDevelopement@gmail.com>
2023-08-13 17:15:28 -0600

    brought in a modal image, created a new admin dashboard component, will get back to this after I change the checkout page design

--- src/Components/New-Arrivals/NewArrivals.js
+++ src/Components/New-Arrivals/NewArrivals.js
@@ -1,47 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Products from '../Products/Products'
-import FilterProducts from '../Utilities/FilterProducts'
-import Data from '../Utilities/Data'
+import React, { useState, useEffect } from "react";
+import Products from "../Products/Products";
+import FilterProducts from "../Utilities/FilterProducts";
+import Data from "../Utilities/Data";
+import ModalImage from "../../Images/Homepage/skin-care-modal-one.jpg";
 
 export default () => {
-  const [data, setData] = useState(Data)
-  const [products, setProducts] = useState(data)
+  const [data, setData] = useState(Data);
+  const [products, setProducts] = useState(data);
 
   const handleFilterClick = (categorie) => {
-    if (categorie === 'all') {
-      return setProducts(data)
+    if (categorie === "all") {
+      return setProducts(data);
     }
 
     const filteredProducts = data.filter(
       (product) => product.categorie === categorie
-    )
+    );
 
-    setProducts(filteredProducts)
-  }
+    setProducts(filteredProducts);
+  };
 
   useEffect(() => {
-    const rootElement = document.getElementById('root')
-    rootElement.scrollIntoView({
-      behavior: "auto",
-      block: "start"
-    }, 500)
-  }, [])
-  
+    const rootElement = document.getElementById("root");
+    rootElement.scrollIntoView(
+      {
+        behavior: "auto",
+        block: "start",
+      },
+      500
+    );
+  }, []);
+
   return (
     <div className="new-arrivals-container">
       <div className="new-arrivals-image-wrapper">
         <img
           className="new-arrivals-image"
-          src="https://via.placeholder.com/250x250"
+          src={ModalImage}
           alt="placeholder"
         />
       </div>
 
-      <FilterProducts handleFilterClick={(product) => handleFilterClick(product)} />
+      <FilterProducts
+        handleFilterClick={(product) => handleFilterClick(product)}
+      />
 
       <div className="new-arrivals-products-container">
         <Products products={products} />
       </div>
     </div>
-  )
-}
+  );
+};
